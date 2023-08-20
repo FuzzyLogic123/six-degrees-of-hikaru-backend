11c94eb6ad700ff22af9aa3484c6c41d66219b8c
Patrick Edwards <pat.e1@icloud.com>
2023-08-21 00:22:26 +1000

    update code slightly

---
+++ instructions.md
@@ -0,0 +1,2 @@
+1. Run get request URLS
+2. 
--- main.py
+++ main.py
@@ -1,39 +1,37 @@
+import json
 from get_request_urls import get_request_urls
 from get_player_losses import get_player_losses
 from process_csv import process_csv
 import csv
 
-root = ["Hikaru"]
-
-DEGREES_LIMIT = 2
+degree = 1
 TIME_CONTROL = "bullet"
-URL_LOCATION = './data/request_urls/1.txt'
-# URL_FAIL_LOCATION = './data/request_urls/fail.txt'
-# LOSSES_SAVE_LOCATION = './data/player_losses/1.jsonl'
-# FAILURES_SAVE_LOCATION = './data/player_losses/failed_requests.jsonl'
+URL_LOCATION = './data/request_urls.txt'
 
+
+# player losses is a dict: key is player, value is a dictionary with one key 'losses' which is a list of strings containing players that they have lost to
 def writeToCsv(player_losses, degree):
-    filename = f'./data/final/1-{TIME_CONTROL}.csv'
-    header = ['username', 'next_player', 'degree']
+    filename = f'./data/{TIME_CONTROL}.csv'
     data = []
     for player_that_lost in player_losses:
         for player_that_won in player_losses[player_that_lost]["losses"]:
             data.append([player_that_won, player_that_lost, degree])
 
     with open(filename, 'a', newline="") as file:
-        csvwriter = csv.writer(file) # 2. create a csvwriter object
         if degree == 1:
-            csvwriter.writerow(header) # 4. write the header
-        csvwriter.writerows(data) # 5. write the rest of the data
+            file.write('username,next_player,degree')
+
+        for line in data:
+            file.write(f'\n{line[0]},{line[1]},{line[2]}')
 
 
-for degree in range(DEGREES_LIMIT):
-    # failed_requests = get_request_urls(root, TIME_CONTROL, URL_LOCATION)
-    # print(failed_requests)
-    player_losses = get_player_losses(URL_LOCATION)
-    writeToCsv(player_losses, degree + 1)
+player_losses = get_player_losses(URL_LOCATION)
+writeToCsv(player_losses, degree)
 
-    root_set = set()
-    for player in player_losses:
-        root_set.update(player_losses[player]["losses"])
-    root = list(root_set)
+root_set = set()
+for player in player_losses:
+    root_set.update(player_losses[player]["losses"])
+root = list(root_set)
+
+with open('./data/root.json', 'w') as file:
+    json.dump(root, file)
